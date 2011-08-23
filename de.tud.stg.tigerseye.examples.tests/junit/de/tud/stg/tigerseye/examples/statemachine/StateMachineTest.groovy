984734cf7ac57708deb5d15803c16136e4ee1373
Leo Roos <leor.email@googlemail.com>
2011-08-23 17:54:43 +0200

    removed unused import statement

--- de.tud.stg.tigerseye.examples.tests/junit/de/tud/stg/tigerseye/examples/statemachine/StateMachineTest.groovy
+++ de.tud.stg.tigerseye.examples.tests/junit/de/tud/stg/tigerseye/examples/statemachine/StateMachineTest.groovy
@@ -5,8 +5,6 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-import de.tud.stg.popart.builder.test.statemachine.OutputAction;
-
 public class StateMachineTest {
 
 	StateMachine m
