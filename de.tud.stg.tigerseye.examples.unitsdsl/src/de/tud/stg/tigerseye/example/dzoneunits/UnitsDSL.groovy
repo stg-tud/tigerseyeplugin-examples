d3143a932e8e77de6e10147bd5e9d6dd924543c7
Leo Roos <leor.email@googlemail.com>
2011-10-20 23:27:47 +0200

    adjusted to dslsupport plug-in as dsldefinitions extension point provider. Made necessary subsequent necessary changes.

--- de.tud.stg.tigerseye.examples.unitsdsl/src/de/tud/stg/tigerseye/example/dzoneunits/UnitsDSL.groovy
+++ de.tud.stg.tigerseye.examples.unitsdsl/src/de/tud/stg/tigerseye/example/dzoneunits/UnitsDSL.groovy
@@ -6,9 +6,6 @@ import org.jscience.physics.amount.Amount;
 import de.tud.stg.popart.builder.core.annotations.DSLParameter;
 import de.tud.stg.popart.builder.core.annotations.DSLMethod;
 import de.tud.stg.popart.dslsupport.Interpreter
-import de.tud.stg.popart.eclipse.core.debug.annotations.PopartType;
-import de.tud.stg.popart.eclipse.core.debug.model.keywords.PopartOperationKeyword;
-import de.tud.stg.tigerseye.eclipse.core.builder.transformers.AnnotationExtractor.DoubleElementHandler;
 
 class UnitsDSL extends Interpreter{
 
@@ -24,7 +21,6 @@ class UnitsDSL extends Interpreter{
 	}
 
 	@DSLMethod(production="p0_kg")
-	
 	public Amount kilogram(Object n){
 		return amountFor(n,"kg");
 	}
@@ -48,43 +44,36 @@ class UnitsDSL extends Interpreter{
 	}
 
 	@DSLMethod(production="p0_cm")
-	
 	public Amount centimeter(Object n){
 		return amountFor(n,"cm")
 	}
 
 	@DSLMethod(production="p0_km")
-	
 	public Amount kilometer(Object n){
 		return amountFor(n,"km")
 	}
 
 	@DSLMethod(production="p0_min")
-	
 	public Amount minutes(Object n){
 		return amountFor(n,"min")
 	}
 
 	@DSLMethod(production="p0_m")
-	
 	public Amount meter(Object n){
 		return amountFor(n,"m")
 	}
 
 	@DSLMethod(production="p0_h")
-	
 	public Amount hours(Object n){
 		return amountFor(n,"h")
 	}
 
 	@DSLMethod(production="p0_s")
-	
 	public Amount seconds(Object n){
 		return amountFor(n,"s")
 	}
 
 	@DSLMethod(production="p0_in")
-	
 	public Amount inch(Object n){
 		return amountFor(n,"in")
 	}
